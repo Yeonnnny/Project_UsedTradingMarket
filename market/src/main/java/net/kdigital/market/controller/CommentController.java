456f406f38ba13d9f52a33dd1a4eeff0e446ff89
Yeonnnny <kdoy030@gmail.com>
2024-03-31 19:07:11 +0900

    240331 5th : boardDetail 뷰에서 댓글 관련 처리 (ajax 사용)

--- market/src/main/java/net/kdigital/market/controller/CommentController.java
+++ market/src/main/java/net/kdigital/market/controller/CommentController.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import net.kdigital.market.dto.CommentDTO;
 import net.kdigital.market.service.CommentService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
